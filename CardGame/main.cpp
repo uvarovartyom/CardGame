1f9a0061e0d67723c5f77bb0c1c47f5f030653ee
Артем Уваров <artemu97@yandex.ru>
2016-05-16 20:39:35 +0500

    Initial Commit

---
+++ CardGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CardGame
+//
+//  Created by Артем Уваров on 16.05.16.
+//  Copyright © 2016 Артем Уваров. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
